6ae5652a1eaf200fe8668ea8c0ac6fc3485da5c3
Rifqi Pinasthika <v-rifqi.pinasthika@traveloka.com>
2023-12-12 12:26:40 +0700

    Update argument write log report

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Traveloka_DataAutomation_UploadBucket
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
